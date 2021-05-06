6f8d9c254dc09df4a14e803159ab940eff2255e8
Orozco23 <jennymlo2399@gmail.com>
2021-05-06 13:18:17 -0600

    Presentaciones, ejemplos de html, css y js

---
+++ Taller 2 - CSS y JS/ejemplo.js
@@ -0,0 +1,31 @@
+function calcularVoltaje () {
+    var resistencia = document.getElementById("resistencia").value;
+    var corriente = document.getElementById("corriente").value;
+    var voltaje = "error";
+    if(corriente != 0){
+        voltaje = Number(resistencia) / Number(corriente);
+    }    
+    document.getElementById("voltaje").value = voltaje;
+}
+
+function suma () {
+    var resistencia = document.getElementById("resistencia").value;
+    var corriente = document.getElementById("corriente").value;
+    var res = Number(resistencia) + Number(corriente);
+    document.getElementById("voltaje").value = res;
+}
+
+function resta () {
+    var resistencia = document.getElementById("resistencia").value;
+    var corriente = document.getElementById("corriente").value;
+    var res = Number(resistencia) - Number(corriente);
+    document.getElementById("voltaje").value = res;
+}
+
+
+function multiplicacion () {
+    var resistencia = document.getElementById("resistencia").value;
+    var corriente = document.getElementById("corriente").value;
+    var res = Number(resistencia) * Number(corriente);
+    document.getElementById("voltaje").value = res;
+}
